e8866d6fb91a6fd4b38968f27bd7751a45f69b86
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-05-11 14:04:16 +0530

    satrt layede desktop application

---
+++ src/controller/CustomerFormController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class CustomerFormController {
+    public void btnSaveCustomer(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateCustomer(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteCustomer(ActionEvent actionEvent) {
+    }
+}
---
+++ src/controller/HomeFormController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class HomeFormController {
+    public AnchorPane MainContext;
+
+    public void btnCustomer(ActionEvent actionEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../views/CustomerForm.fxml"));
+        Scene scene = new Scene(load);
+        Stage stage = (Stage) MainContext.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
