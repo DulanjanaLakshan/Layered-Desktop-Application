081763a1d6b99e3fa7ea3be297090f82fe1b0d7f
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-05-18 00:11:17 +0530

    start backend

--- src/controller/CustomerFormController.java
+++ src/controller/CustomerFormController.java
@@ -1,9 +1,11 @@
 package controller;
 
+import Dto.CustomerDTO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -12,8 +14,21 @@ import java.io.IOException;
 
 public class CustomerFormController {
     public AnchorPane mainContext;
+    public TextField txtCustomerId;
+    public TextField txtCustomerName;
+    public TextField txtCustomerAddress;
+    public TextField txtCustomerNo;
+    public TextField txtCustomerEmail;
+
+    String id=txtCustomerId.getText();
+    String name=txtCustomerName.getText();
+    String address=txtCustomerAddress.getText();
+    String no=txtCustomerNo.getText();
+    String email=txtCustomerEmail.getText();
+
 
     public void btnSaveCustomer(ActionEvent actionEvent) {
+        CustomerDTO customerDTO = new CustomerDTO(id, name, address, no, email);
     }
 
     public void btnUpdateCustomer(ActionEvent actionEvent) {
